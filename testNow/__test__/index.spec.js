4ecf4aa9aff65e4ee4c159d2fdd460a5675f0b08
lirl <lironglian@ewell.cc>
2020-07-20 18:56:25 +0800

    生成测试程序

--- testNow/__test__/index.spec.js
+++ testNow/__test__/index.spec.js
@@ -1,10 +1,10 @@
-// test ('测试生成文件名', () => {
-//   const src = new(require('../index'))()
-//   const ret = src.getFileName('/abc/class.js')
-//   console.log('getFileName:', ret)
-//   expect(ret)
-//     .toBe('/abc/__test__/class.spec.js')
-// })
+test ('测试生成文件名', () => {
+  const src = new(require('../index'))()
+  const ret = src.getFileName('/abc/class.js')
+  console.log('getFileName:', ret)
+  expect(ret)
+    .toBe('/abc/__test__/class.spec.js')
+})
 
 test('测试代码生成', () => {
   const src = new (require('../index'))()
@@ -16,7 +16,18 @@ test('测试代码生成', () => {
         const fun = require('../class')
         const ret = fun()
         // expect(ret)
-          .toBe('test return')
+        //  .toBe('test return')
       })
     `)
+})
+
+const fs = require('fs')
+test('集成测试生成代码文件', () => {
+  // 准备环境
+  // 删除文件夹
+  fs.rmdirSync(__dirname + '/data/__test__', {
+    recursive: true // 迭代，如果有就删除
+  })
+  const src = new (require('../index'))()
+  src.genJestSource(__dirname + '/data')
 })
--- testNow/index.js
+++ testNow/index.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 
 module.exports = class TextNow {
   /**
@@ -30,8 +31,42 @@ module.exports = class TextNow {
         const ${isClass ? '{' + methodName + '}' : methodName} = require('${'../' + classFile}')
         const ret = ${methodName}()
         // expect(ret)
-          .toBe('test return')
+        //  .toBe('test return')
       })
     `
+  }
+
+  genJestSource(sourcePath = path.resolve('./')) {
+    const testPath = `${sourcePath}/__test__`
+    if(!fs.existsSync(testPath)) {
+      fs.mkdirSync(testPath)
+    }
+    // 遍历代码文件
+    let list = fs.readdirSync(sourcePath)
+    list.map(v => `${sourcePath}/${v}`) // 添加完整路径
+      .filter(v => fs.statSync(v).isFile())  // 过虑文件
+      .filter(v => v.indexOf('.spec') === -1) // 排除测试文件
+      .map(v => this.genTestFile(v))
+  }
+  genTestFile (filename) {
+    console.log('filename', filename)
+    const testFileName = this.getFileName(filename)
+
+    // 判断此文件是否存在
+    if (fs.existsSync(testFileName)) {
+      console.log('该测试代码已存在')
+      return
+    }
+    const mod = require(filename)
+    let source
+    if (typeof mod === 'object') {
+      source = Object.keys(mod)
+      .map(v => this.getSource(v, path.basename(filename), true))
+      .join('\n') // 给每行代码后面加上空格
+    } else if (typeof mod === 'function') {
+      const baseName = path.basename(filename)
+      source = this.getSource(baseName.replace('.js', ''), baseName)
+    }
+    fs.writeFileSync(testFileName, source)
   }
 }
